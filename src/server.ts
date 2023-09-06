36f618d1287431e205f91249e9b5e99ca2008281
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-06 11:04:20 -0300

    feat: Instaled eslint and fongi this rocketseat and ajusted project.

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
-const app = fastify();
+const app = fastify()
 
 // GET, POST, PUT, PATCH, DELETE
 
 // http://localhost:333/hello
 
-app.get("/hello", () => {
-  return "Hello World";
-});
+app.get('/hello', () => {
+  return 'Hello World'
+})
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP Server Runing!");
-  });
+    console.log('HTTP Server Runing!')
+  })
