e2e8080574e59dd387477e53844c4f6da897f549
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-11 14:05:28 -0300

    feat: Created Test.

--- src/test/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('o usuário consegue cirar uma nova transação', () => {
-  const responseStatuscode = 201
-
-  expect(responseStatuscode).toEqual(201)
-})
